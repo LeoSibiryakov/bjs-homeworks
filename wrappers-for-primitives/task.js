add364f372cc6b3d8410fcf3ba85220a238c51f4
Leonid Sibiryakov <leo-sibiryakov@yandex.ru>
2019-11-20 20:30:03 +0500

    change

--- wrappers-for-primitives/task.js
+++ wrappers-for-primitives/task.js
@@ -1,3 +1,5 @@
+"use strict"
+
 function calculateMortgage() {
     let percent = window.percent.value;
     let contribution = window.contribution.value;
@@ -12,8 +14,27 @@ function calculateMortgage() {
 function calculateTotalMortgage(percent, contribution, amount, date) {
 
     // код для задачи №1 писать здесь
-    //return totalAmount;
+    //return totalAmount; 
+  let credit = [];
+
+  for (let i = 0; i < arguments.length - 1; i++) {
+    credit[i] = parseInt(arguments[i], 10);
+    if (isNaN(arguments[i])) {
+      return `Параметр ${i + 1} содержит неправильное значение ${arguments[i]}`;
+    }
+  }
+
+let S = amount - contribution;
+let P = (percent / 100) / 12;
+let newdate = new Date();
+let end = new Date(date);
+let n = ((end.getMonth() - newdate.getMonth()) + (12 * (end.getFullYear() - newdate.getFullYear())));
+let pay = S * (P + P / (((1 + P) ^ n)-1));
+let totalAmount = (n * pay).toFixed(2);
+console.log(totalAmount);
+return totalAmount;
 }
+
 
 function sayHello() {
     let name = window.personName.value;
@@ -25,4 +46,13 @@ function sayHello() {
 function getGreeting(name) {
     // код для задачи №2 писать здесь
     //return greeting;
-}
+
+    if (name == null || name == undefined || name == Number(name)) {
+        name = 'Аноним';
+        console.log(`Привет, мир! Меня зовут ${name}`);
+        return `Привет, мир! Меня зовут ${name}`;
+      } else { 
+        console.log(`Привет, мир! Меня зовут ${name}`);
+        return `Привет, мир! Меня зовут ${name}`;
+      }
+    }
