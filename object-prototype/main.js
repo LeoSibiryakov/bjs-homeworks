7dc1c162ef823def801ef701afd5fe6eeceaef6c
Leonid Sibiryakov <leo-sibiryakov@yandex.ru>
2019-11-24 11:45:33 +0500

    first commit

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -8,7 +8,14 @@ function initCheckBirthday() {
 
 function checkBirthday(birthday) {
     // код для задачи №1 писать здесь
+    let now = Date.now(new Date());
+    let dateOfBirthday = +new Date(birthday);
+    console.log(dateOfBirthday);
+    let diff = now - dateOfBirthday;
+    let age = diff / (1000 * 60 * 60 * 24 * 365.25);
+    return age >= 18 ? true : false;
 }
+
 
 function initPrintAnimalSound() {
     const animal = {
@@ -22,7 +29,9 @@ function initPrintAnimalSound() {
 
 function getAnimalSound(animal) {
     // код для задачи №2 писать здесь
-}
+    let sound = animal.sound;
+    return sound === undefined ? null : sound;
+    }
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
@@ -36,4 +45,13 @@ function initCalculateStatement() {
 
 function getAverageMark(marks) {
     // код для задачи №3 писать здесь
+    let arrayMarks = [];
+    let roundedAverage = 0;
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        arrayMarks[i] = parseInt(marks[i], 10);
+        sum += arrayMarks[i];
+    }
+    return roundedAverage = Math.round(sum / arrayMarks.length);
 }
